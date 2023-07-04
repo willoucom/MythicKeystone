029a0a640b7d139c65ef147e405f055c616f432f
Wilfried JEANNIARD <willou.com@gmail.com>
2023-07-04 13:20:53 +0200

    Feat: add zhTW localization

---
+++ Locales/zhTW.lua
@@ -0,0 +1,13 @@
+local myname, ns = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(myname, "zhTW", false)
+if not L then return end
+
+L["AddonName"] = "傳奇鑰石"
+L["Alts"] = "其他角色"
+L["Countdown"] = "倒數"
+L["Countdown_cancel"] = "取消"
+L["Group"] = "隊伍"
+L["Guild"] = "公會"
+L["ReadyCheck"] = "準備確認"
+L["Refresh"] = "點擊來刷新。"
