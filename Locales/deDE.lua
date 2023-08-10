14df5ee722e49742a4d04031e934fc4a33f86063
Wilfried JEANNIARD <willou.com@gmail.com>
2023-08-10 11:26:40 +0200

    Feat: add deDE localization

---
+++ Locales/deDE.lua
@@ -0,0 +1,12 @@
+local myname, ns = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(myname, "deDE", true)
+
+L["AddonName"] = "Mythic Keystones"
+L["Alts"] = "Figuren"
+L["Countdown"] = "Countdown"
+L["Countdown_cancel"] = "beenden"
+L["Group"] = "Gruppe"
+L["Guild"] = "Gilde"
+L["ReadyCheck"] = "Bereitschaftsprüfung"
+L["Refresh"] = "Klicken Sie zum Aktualisieren."
