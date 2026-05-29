9fae866c3835b2f5386a63d4f77d437d64dfc733
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-05-30 04:25:11 +0500

    Create RU locale

---
+++ MythicKeystone_LibDataBroker/Locales/ruRU.lua
@@ -0,0 +1,8 @@
+local myname, ns = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(myname, "ruRU", false)
+if not L then return end
+
+L["AddonName"] = "Mythic Keystones"
+L["Alts"] = "Альты"
+L["Guild"] = "Гильдия"
