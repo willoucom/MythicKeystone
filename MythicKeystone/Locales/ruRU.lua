8b7df46dc06be3f21e09eefccb448616819bb09e
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-05-30 04:22:13 +0500

    Create RU locale
    Added Russian localization for MythicKeystone addon.

---
+++ MythicKeystone/Locales/ruRU.lua
@@ -0,0 +1,36 @@
+local myname = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(myname, "ruRU", false)
+if not L then return end
+
+L["Ready"] = "Готовы?"
+L["DungeonCountdown"] = "MythicKeystone: Активация ключа через 5 секунд, пристегнитесь и приготовьтесь."
+L["DungeonStarted"] = "MythicKeystone: Ключ вставлен, желаем удачного прохождения, старайтесь изо всех сил и остерегайтесь огня!"
+L["DungeonCountdownStop"] = "MythicKeystone: Активация ключа остановлена."
+
+L["AddonName"] = "Mythic Keystones"
+L["Alts"] = "Альты"
+L["Guild"] = "Гильдия"
+L["Group"] = "Группа"
+L["Refresh"] = "Кликните для обновления."
+L["ReadyCheck"] = "Проверка готовности"
+L["Countdown"] = "Обратный отсчёт"
+L["Countdown_cancel"] = "Отмена"
+
+-- About popup
+L["ABOUT_btn_tooltip"] = "Об этом аддоне"
+L["ABOUT_title"] = "О MythicKeystone"
+L["ABOUT_author"] = "Автор: Willou"
+L["ABOUT_description"] = "Ваши ключи, ключи Ваших альтов, ключи Вашей группы и ключи Вашей гильдии - всё во фрейме поиска группы (LFG), в реальном времени."
+L["ABOUT_other_header"] = "Также загляните в Mythic Dungeon Notes"
+L["ABOUT_other_pitch"] = "Все стратегии для подземелий и заметки к боссам, в игре и в один клик."
+L["ABOUT_other_feature_1"] = "• Все M+ подземелья"
+L["ABOUT_other_feature_2"] = "• Стратегии боссов, советы по трешу, прерывания"
+L["ABOUT_other_feature_3"] = "• Автоопределение зоны открывает нужное руководство"
+L["ABOUT_other_feature_4"] = "• Встроенные кнопки телепортации"
+L["ABOUT_other_companion"] = "Советы для новичков и опытных игроков выделены цветом, чтобы опытные игроки могли пропускать основы."
+L["ABOUT_links_header"] = "Ссылки"
+L["ABOUT_link_curseforge"] = "CurseForge"
+L["ABOUT_link_wago"] = "Wago"
+L["ABOUT_link_github"] = "GitHub"
+L["ABOUT_url_popup_text"] = "Скопируйте этот URL с помощью Ctrl+C:"
